9751bf3a6520ac2ab06baf8e0faebaff22814787
castagnarodavid9-sketch <castagnarodavid9@gmail.com>
2025-12-16 09:48:33 -0300

    Criada atividade de cálculo de juros.

--- _juros.rb
+++ _juros.rb
@@ -1,7 +1,30 @@
+require 'date'
+
 =begin
-  3.	Faça um programa que a partir de um valor e de uma data de vencimento, calcule o valor dos juros na data de hoje considerando que a multa seja de 2,5% ao dia.
+3. Faça um programa que a partir de um valor e de uma data de vencimento,
+calcule o valor dos juros na data de hoje considerando que a multa seja de 2,5% ao dia.
 =end
 
-puts "="*50
+puts "=" * 50
 puts "Cálculo de juros da Target Sistemas"
-puts "="*50
+puts "=" * 50
+
+puts "Informe o valor da prestação:"
+prest = gets.chomp.to_f
+
+puts "Informe a data de vencimento (dd/mm/aaaa):"
+data_venc = Date.strptime(gets.chomp, "%d/%m/%Y")
+
+data_hoje = Date.today
+
+if data_hoje > data_venc
+  dias_atraso = (data_hoje - data_venc).to_i
+  juros = prest * 0.025 * dias_atraso
+  total = prest + juros
+
+  puts "\nDias em atraso: #{dias_atraso}"
+  puts "Valor dos juros: R$ #{'%.2f' % juros}"
+  puts "Valor total a pagar: R$ #{'%.2f' % total}"
+else
+  puts "\nPagamento em dia. Valor a pagar: R$ #{'%.2f' % prest}"
+end
